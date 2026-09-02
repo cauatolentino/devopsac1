d9ea1f346bb9bb99d5cec467b6484fcd0cbbca47
Caua Tolentino <cauatolentino@hotmail.com>
2026-09-02 19:12:44 -0300

    chore: projeto base Spring Boot gerado no Spring Initializr
    Estrutura inicial feita em aula com a professora (02/09).
    Dependencias: Spring Web (MVC), Spring Data JPA, H2, PostgreSQL.
    
    Co-authored-by: Pedro Pizzi <118937963+Pedro-VPizzi@users.noreply.github.com>
    Co-authored-by: HenryTanaka <henry.tanaka12@outlook.com.br>

---
+++ src/main/java/com/example/grupo12_praticaatdd/Grupo12PraticaAtddApplication.java
@@ -0,0 +1,13 @@
+package com.example.grupo12_praticaatdd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Grupo12PraticaAtddApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Grupo12PraticaAtddApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/grupo12_praticaatdd/Grupo12PraticaAtddApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grupo12_praticaatdd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Grupo12PraticaAtddApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
