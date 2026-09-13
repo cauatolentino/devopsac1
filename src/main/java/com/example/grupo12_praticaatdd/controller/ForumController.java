2a9134ef47fd93a890547fab2194e228098d1645
Caua Tolentino <cauatolentino@hotmail.com>
2026-09-13 15:30:22 -0300

    feat: perfis de banco H2 e PostgreSQL
    Perfil padrao (e perfil 'h2') usa H2 em memoria com console em /h2-console.
    Perfil 'postgres' aponta para o PostgreSQL, com host, porta, base, usuario e
    senha vindos de variaveis de ambiente para funcionar no Docker Compose.
    
    @CrossOrigin no controller para o front-end VueJS consumir a API.

--- src/main/java/com/example/grupo12_praticaatdd/controller/ForumController.java
+++ src/main/java/com/example/grupo12_praticaatdd/controller/ForumController.java
@@ -13,6 +13,7 @@ import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -27,6 +28,7 @@ import java.util.List;
  */
 @RestController
 @RequestMapping("/api")
+@CrossOrigin(origins = "*")
 @Tag(name = "Forum", description = "US1 - premiacao do aluno com maior engajamento no forum")
 public class ForumController {
 
